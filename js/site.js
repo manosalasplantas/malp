c2b5a7f05f912b3069bcfe1bebee2a4626663e77
manosalasplantas <smontero235@gmail.com>
2025-08-21 21:11:00 -0300

    Mover archivos a la raíz
    subir a la web

---
+++ js/site.js
@@ -0,0 +1,33 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    const carouselItems = document.querySelectorAll('.carrousel-item');
+    let currentItem = 0;
+
+    function showNextItem() {
+        // Oculta el item actual
+        carouselItems[currentItem].style.opacity = '0'; // Cambiamos de 'display: none' a 'opacity: 0' para la transición
+
+        // Pasa al siguiente item
+        currentItem = (currentItem + 1) % carouselItems.length;
+
+        // Muestra el nuevo item con un poco de retraso para que se vea el efecto de transición
+        setTimeout(() => {
+            carouselItems[currentItem].style.opacity = '1';
+        }, 50);
+    }
+
+    // Oculta todos los items excepto el primero
+    carouselItems.forEach((item, index) => {
+        if (index !== 0) {
+            item.style.opacity = '0';
+        } else {
+            item.style.opacity = '1';
+        }
+    });
+
+    // Inicia el carrusel
+    setInterval(showNextItem, 3000); // Cambia de foto cada 3 segundos (3000 milisegundos)
+});
